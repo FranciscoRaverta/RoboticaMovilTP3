a77ea1d35752e5b8f64a4403a7f1a35d1b30eca1
Francisco <francisco.raverta@gmail.com>
2023-10-12 15:51:54 -0300

    Quise agregar el último punto, pero me marié sola y no me salió u.u . Hay que repensarlo. Tania

--- volume/rect_img.py
+++ volume/rect_img.py
@@ -76,6 +76,21 @@ def plotCameraPoses(R1, t1, R2, t2, count):
     plt.close()
     return
 
+def plotCameraTrajectory(R_list, t_list, count):
+
+    # # Aplicamos la transformación relativa para obtener la pose de la segunda cámara
+    # R2 = np.dot(R2, R1)  # Combinamos las rotaciones
+    # t2 = t2 + np.dot(R1, t2)  # Combinamos las traslaciones
+    
+    visualizer = CameraPoseVisualizer([-5, 5], [-5, 5], [-5, 5])
+    for ind, _ in enumerate(R_list):
+        P = np.vstack((np.hstack((R_list[ind],t_list[ind])), np.array([0,0,0,1])))
+        visualizer.extrinsic2pyramid(P, 'c', 1)
+    # Guardar plot
+    plt.savefig(f"images/trajectory/trayectoria_{count}.png")
+    plt.close()
+    return
+
 
 class ImgProcessing(Node):
     def __init__(self):
@@ -87,7 +102,9 @@ class ImgProcessing(Node):
 
         self.proj_left, self.K_left, self.dist_coeff_left = ReadYaml('data/left.yaml')
         self.proj_right, self.K_right, self.dist_coeff_right = ReadYaml('data/right.yaml')
-
+        self.distance_between_cameras = abs(self.proj_right[0,3])
+        self.focal_length_of_camera =(self.K_left[0,0]+self.K_left[1,1])/2
+        
         # Creamos el objeto ts del tipo TimeSynchronizer encargado de sincronizar los mensajes recibidos.
         ts = message_filters.TimeSynchronizer([self.left_rect, self.right_rect], 10)
 
@@ -98,7 +115,7 @@ class ImgProcessing(Node):
         self.bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True) 
         self.count = 0
         self.br = CvBridge() # Para convertir de mensaje de imagen de ROS a imagen de opencv
-
+        
         self.t_left = []
         self.R_left = []
         
@@ -109,34 +126,30 @@ class ImgProcessing(Node):
         img_right = self.br.imgmsg_to_cv2(right_msg)
 
         # find the keypoints with ORB
-        kp_left = self.orb.detect(img_left, None)
+        keypoints_left = self.orb.detect(img_left, None)
         # compute the descriptors with ORB
-        kp_left, des_left = self.orb.compute(img_left, kp_left)
+        keypoints_left, des_left = self.orb.compute(img_left, keypoints_left)
 
         # find the keypoints with ORB
-        kp_right = self.orb.detect(img_right, None)
+        keypoints_right = self.orb.detect(img_right, None)
         # compute the descriptors with ORB
-        kp_right, des_right = self.orb.compute(img_right, kp_right)
+        keypoints_right, des_right = self.orb.compute(img_right, keypoints_right)
 
         # Compute matches between images
         matches = self.bf.match(des_left, des_right) 
         matches = sorted(matches, key = lambda x:x.distance)
 
-        img_matches = cv.drawMatches(img_left,kp_left,img_right,kp_right,matches[:],outImg=None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        img_matches = cv.drawMatches(img_left,keypoints_left,img_right,keypoints_right,matches[:],outImg=None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
         #cv.imwrite(f"images/matches_{self.count}.png", img_matches)
 
-        img_matches = cv.drawMatches(img_left,kp_left,img_right,kp_right,matches[:30],outImg=None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        img_matches = cv.drawMatches(img_left,keypoints_left,img_right,keypoints_right,matches[:30],outImg=None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
         #cv.imwrite(f"images/30matches_{self.count}.png", img_matches)
 
 
         ''' Triangulation '''
         # Convert keypoints to numpy arrays of pixel coordinates
-        points_left = np.array([kp_left[m.queryIdx].pt for m in matches], dtype=np.float32)
-        points_right = np.array([kp_right[m.trainIdx].pt for m in matches], dtype=np.float32)
-
-        # Ensure that the points are in homogeneous coordinates
-        points_left_homogeneous = np.hstack((points_left, np.ones((points_left.shape[0], 1))))
-        points_right_homogeneous = np.hstack((points_right, np.ones((points_right.shape[0], 1))))
+        points_left = np.array([keypoints_left[m.queryIdx].pt for m in matches], dtype=np.float32)
+        points_right = np.array([keypoints_right[m.trainIdx].pt for m in matches], dtype=np.float32)
 
         points4d = cv.triangulatePoints(self.proj_left, self.proj_right, points_left.T, points_right.T)
         points3d = (points4d[:3, :]/points4d[3, :]).T
@@ -180,6 +193,7 @@ class ImgProcessing(Node):
 
         R1, R2, P1, P2, Q, _, _ = cv.stereoRectify(self.K_left, self.dist_coeff_left, self.K_right, self.dist_coeff_right,img_left.shape,R,t) 
         #camera_matrix1, distCoeffs1, camera_matrix2, distCoeffs2, img, R, T 
+        print("Q = ", Q)
         reprojection = cv.reprojectImageTo3D(disparity_map, Q)
         #cv.imwrite(f"images/reprojection_{self.count}.png", reprojection)
         
@@ -199,23 +213,65 @@ class ImgProcessing(Node):
 
         '''Estimación trayectoria cámara left'''
         if self.count == 0:
-            self.t_left.append(np.zeros((3,1)))
-            self.R_left.append(np.eye(3))
+            tvec = np.zeros((3,1))
+            R = np.eye(3)
         else:
-            H, mask = cv.findHomography(points_left, points_right, cv.RANSAC,5.0)
-            # La máscara es un vector de 1's y 0's que dicen qué puntos fueron filtrados y cuáles no. Hay que aplicar esa máscara a los vectores de puntos 2d y 3d
-            #print(mask)
-            #print(mask.shape)
-            inlier_mask = mask.ravel() == 1
-            points_left_inliers = points_left[inlier_mask]
-            points_right_inliers = points_right[inlier_mask]
-            E, _ = cv.findEssentialMat(points1=points_left_inliers, points2=points_right_inliers, cameraMatrix1=self.K_left, distCoeffs1=self.dist_coeff_left, 
-                        cameraMatrix2=self.K_right, distCoeffs2=self.dist_coeff_right)
-            # E, R y t se le dan a cv.RecoverPose como estimaciones iniciales, y después la función devuelve los valores finales estimados.
-            _, E, R, t, _ = cv.recoverPose(points1=points_left_inliers, points2=points_right_inliers, cameraMatrix1=self.K_left, distCoeffs1=self.dist_coeff_left, 
-                                            cameraMatrix2=self.K_right, distCoeffs2=self.dist_coeff_right, E=E, R=R, t=t, method=cv.RANSAC)
-    
+            # find the keypoints with ORB
+            keypoints_left_prev = self.orb.detect(self.img_left_prev, None)
+            # compute the descriptors with ORB
+            keypoints_left_prev, des_left_prev = self.orb.compute(img_right, keypoints_left_prev)
+
+            # Compute matches between images
+            matches = self.bf.match(des_left, des_left_prev) 
+            matches = sorted(matches, key = lambda x:x.distance)
+
+            img_matches = cv.drawMatches(img_left,keypoints_left,self.img_left_prev,keypoints_left_prev,matches[:],outImg=None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+            #cv.imwrite(f"images/matches_entre_frames_{self.count}.png", img_matches)
+
+            # Convert keypoints to numpy arrays of pixel coordinates
+            points_left = np.array([keypoints_left[m.queryIdx].pt for m in matches], dtype=np.float32)
+            points_left_prev = np.array([keypoints_left_prev[m.trainIdx].pt for m in matches], dtype=np.float32)
+
+            points_left_homogeneous = np.hstack((points_left, np.ones((points_left.shape[0], 1))))
+            points_left_prev_homogeneous = np.hstack((points_left_prev, np.ones((points_left_prev.shape[0], 1))))
+            
+            retval, rvec, tvec, inliers = cv.solvePnPRansac(objectPoints=self.points_3d, imagePoints=points_left, cameraMatrix=self.K_left, distCoeffs=self.dist_coeff_left)
+            R, _ = cv.Rodrigues(rvec)
+        self.t_left.append(tvec.reshape((3, 1)))
+        self.R_left.append(R)
+        plotCameraTrajectory(self.R_left, self.t_left, self.count)
+
+        self.img_left_prev = img_left
+        self.P1_prev = P1
         
+        # El sig código era para armar ahora los puntos 3D para los puntos 2D de la cámara izq para usar en la próxima iteración, pero está mal, porque
+        # en la próxima iteración son otros los puntos 2D para los que quiero el valor en 3D u.u . REPENSAR
+
+        # Define the baseline (distance between the two cameras) and focal length
+        baseline = self.distance_between_cameras
+        focal_length = self.focal_length_of_camera
+        # Iterate over the 2D points in points_left
+        for point_2d in points_left:
+            # Coordinadas del punto en 2D
+            x = point_2d[0]
+            y = point_2d[1]
+
+            # Disparity del pixel correspondiente
+            disparity = disparity_map[int(y), int(x)]
+            # Profundidad según la fórmula de disparidad:
+            depth = (baseline * focal_length) / disparity
+            # Punto 3D en coordenadas de la cámara
+            point_3d = np.array([[x], [y], [depth]])
+            # Punto 3D en coordenadas globales
+            point_3d = R * point_3d + tvec.reshape((3, 1))
+
+            # Agregamos el punto 3D a points_3d
+            if points_3d == None:
+                points_3d = point_3d.T
+            else:
+                points_3d = np.vstack((points_3d,point_3d.T))
+        print(points_3d)
+        self.points_3d = points_3d
         
         self.count += 1
 
